a155956e469548b4408aac868af1d77a484a739c
João Américo <jcamerico@gmail.com>
2023-05-12 14:39:20 +0200

    Update forecast.js (#14)
    Update deadline

--- src/server/routes/forecast.js
+++ src/server/routes/forecast.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const Forecast = require('../models/forecast');
-const limitDate = new Date('2023-05-12');
+const limitDate = new Date('2023-05-13');
 const deadlineErrorMessage = 'Sorry, but the deadline for submitting new forecasts is over! Sashay away!';
 
 router.post('/', function(req, res) {
     const forecast = new Forecast(req.body);
     const now = new Date();
-    if (now > limitDate) {
+    if (now >= limitDate) {
         console.log(deadlineErrorMessage);
         res.status(400).send(deadlineErrorMessage);
     } else {
